a93f86db965529b1d6fe30d51e099fcf0cc3dbae
LukaszSmoczyk <lukaszsmoczyk@gmail.com>
2021-04-04 01:13:48 +0200

    added _layout and basic views

--- ShopList/Startup.cs
+++ ShopList/Startup.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Razor.Compilation;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
